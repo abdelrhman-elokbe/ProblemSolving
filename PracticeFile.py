d7afaea284c9075452cab2c45182ad049c7c67c3
Abdelrhman Elokbe <abdelrhmanokbe@gmail.com>
2024-12-29 14:03:52 +0200

    1st commit

---
+++ PracticeFile.py
@@ -0,0 +1,10 @@
+myTuple = (10, 20, ("abdo", "ahmed"), 30.5)
+#           0   1          2           3
+
+"""print(myTuple[0])      # 10
+print(myTuple[2][1])   # ahmed
+print(myTuple[2][0][1])# b"""""
+
+myList = [10, 20, ("abdo", "ahmed"), 30.5]
+myList.extend(["samy", "ali"])
+print(myList[4][0])
